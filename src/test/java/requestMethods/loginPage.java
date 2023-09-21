69f12b386e44cb3d51dfd3ab2cfcc22ef4b36b89
saleem ahmad <saleem.ahmad465@gmail.com>
2023-09-21 13:05:50 +0530

    Add search and login pages

---
+++ src/test/java/requestMethods/loginPage.java
@@ -0,0 +1,7 @@
+package requestMethods;
+
+public class loginPage {
+	public void login_Page() {
+		System.out.println("Login Page");
+	}
+}
---
+++ src/test/java/requestMethods/searchPage.java
@@ -0,0 +1,10 @@
+package requestMethods;
+
+import org.testng.annotations.Test;
+
+public class searchPage {
+	@Test
+	public void searchData() {
+		System.out.println("Hello World");
+	}
+}
